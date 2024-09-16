71981366f5331f6d6084872b58b1ce5380fda71e
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-16 13:26:08 +0000

    variables basics done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,30 @@
+// Js variables
+
+// const is used to declare the constant
+const accountID = 21336
+
+// let is used to declare the variable
+let holdername = "shreyansh"
+
+// prefer to not use var because of the issue in blockspace and functional space
+var accountpassword = "6666"
+
+//we can ever declare the variable without using any datatype or keyword for it 
+accountcity = "baran"
+
+let accountstate;
+
+// accountID = 2 
+
+
+// const can't be redeclared neither re assign
+// console.log(accountID);  //not allowed
+
+holdername = "samarth"
+accountpassword = "8888"
+accountcity = "jaipur"
+
+console.table([accountID,holdername,accountpassword,accountcity,accountstate])
+
+
+
