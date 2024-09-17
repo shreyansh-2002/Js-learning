c3178ddf0c481dfb7dcee7351101f8db9a2c781b
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-17 13:50:59 +0000

    datatype conversion done

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,30 @@
+// let age  = "22abc"
+// let score  = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 and false -> 0
+
+let LoggenIn = 0
+let  booleanIsLoggedIn = Boolean(LoggenIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true and 0 => false
+// "" -> false
+// "shrey" -> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
