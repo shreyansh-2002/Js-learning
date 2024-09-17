6dc3df75b42b44f1941de0238fbe41b2860d4aa3
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-17 05:42:50 +0000

    datatype done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+//datatypes in js 
+"use strict"; // treat all code as a new version of JS 
+
+// alert("3+3") // we are using node.js not browser
+
+console.log(3       +        
+    3       );  // not a good practice code readibilty should be high
+    // JS automaticallly avoids spaces 
+
+    let name = "shreyansh"
+    let age = 20
+    let isloggedIn = false
+
+    // number -> 2 to power 53
+    // bigint -> used to store too large numbers such market value in stock market
+    // string -> " "
+    //boolean -> true/false
+    // null -> standalone value
+    // undefined -> 
+    // symbol -> unique things identification
+
+    //object
+
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof undefined);
+
+    //null datatype is object 
+    console.log(typeof null);
+    
+
