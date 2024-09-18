14bdd8130dabacbc794c132f1709e31ba60fb35d
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-18 15:58:10 +0000

    Comparison operators done

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -14,17 +14,94 @@
 
 let LoggenIn = 0
 let  booleanIsLoggedIn = Boolean(LoggenIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // 1 => true and 0 => false
 // "" -> false
 // "shrey" -> true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************************operations*********************************************
+
+// let value  = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " world"
+// console.log(str1+str2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+
+// //operations precedence are done from the left to right
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter)
+
+// *******************comparison of datatype**************************
+
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// *******this is more confusing so don't try to do this**************
+
+/*console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// null -> it converts the null to 0 value 
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+*/
+
+// == -> this compares only the value by converting into the datatype
+// === -> this operator checks the the datatype and value both 
+
+console.log("2" === 2);
+console.log(3 === 3);
+
+
+
+
+
+
+
+
 
 
