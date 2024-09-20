f485c6bbe556c01e858d102b07d69cb317d145dc
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-20 20:51:03 +0000

    dates started

---
+++ 01_basics/08_date_and_time.js
@@ -0,0 +1,13 @@
+// dates
+
+let mydate = new Date();
+console.log(mydate)
+console.log(mydate.toString())
+console.log(mydate.toLocaleString())
+console.log(mydate.toLocaleDateString())
+console.log(mydate.toTimeString())
+console.log(typeof mydate)
+
+let createdate = new Date(2023, 11, 2)
+console.log(createdate)
+console.log(createdate.toLocaleDateString())
