6678729d0e5a5d4600db98d0e93dba7d4a574ca8
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-20 13:04:20 +0000

    strings complete

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -12,16 +12,23 @@ const gameName = new String(`shreyansh`)
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
+
+//length is used to find size of the string
 console.log(gameName.length);
 
 console.log(gameName.toUpperCase());
 
+// char at is used to find the char at given index
 console.log(gameName.charAt(3));
+
+// indexof is used to character at given position
 console.log(gameName.indexOf('h'));
 
 const gamestring = gameName.substring(0,3)
 console.log(gamestring);
 
+
+// slice can take neg values but substring not
 const anotherstring = gameName.slice(-8,3)
 console.log(anotherstring);
 
@@ -30,6 +37,15 @@ console.log(newstr);
 
 const trimstr = newstr.trim()
 console.log(trimstr);
+
+const url = "https:/shrey.com/shreyansh%20somani"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('somani'));
+
+// split function is used to put values in an array
+console.log(gameName.split('-'));
 
 
 
