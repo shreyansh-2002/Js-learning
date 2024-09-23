59ddb11ff1c952dd9ca63af2f2373d73062bcf5f
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-23 21:20:28 +0000

    doing arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,12 +1,17 @@
-const marvel = ["thor","ironman","spiderman"]
-const dc_heros = ["superman","flash","batman"]
+const marvel = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
 
 marvel.push(dc_heros);
 
 console.log(marvel);
 console.log(marvel[3][1]);
 
-const allheros = [...marvel,...dc_heros]
+
+// ... -> this operator is used to add arrays
+const allheros = [...marvel, ...dc_heros]
 console.log(allheros);
+
+console.log();
+
 
 
