240144917b027578afa5a4c78f15587cad96e7d9
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-24 10:05:42 +0000

    array finished

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,17 +1,45 @@
 const marvel = ["thor", "ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel.push(dc_heros);
+// push operation manipulates the data in the same array
+// marvel.push(dc_heros);
 
-console.log(marvel);
-console.log(marvel[3][1]);
+// console.log(marvel);
+// console.log(marvel[3][1]);
 
 
-// ... -> this operator is used to add arrays
-const allheros = [...marvel, ...dc_heros]
+// concat creates the copy of the manipulated array 
+const allheros = marvel.concat(dc_heros);
+
+
+// ...(spread operator) -> this operator is used to spread out things 
+// const allheros = [...marvel, ...dc_heros]
 console.log(allheros);
 
-console.log();
+const newarr = [2, 3, 4, 5, [56, 6, 7], 8, 4, [4, 6, [7, 9, 2]]]
+
+//in flat function we have to decide the depth 
+const anotherarr = newarr.flat(Infinity)
+console.log(anotherarr)
+
+// it s used to check the if given input is array or not 
+console.log(Array.isArray("shreyansh"))
+
+// it id used to make  the array from given input 
+console.log(Array.from("shreyansh"))
+console.log(Array.from({ name: "shrey" }))
+
+let scr1 = 34
+let scr2 = 45
+let scr3 = 56
+
+
+// this method is used to make the array of the data 
+console.log(Array.of(scr1, scr2, scr3))
+
+
+
+
 
 
 
