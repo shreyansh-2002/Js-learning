62b9bdfd08f4b1abb65f1d6c7da3225eadc811db
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2024-09-28 06:47:02 +0000

    scope finished

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -29,3 +29,30 @@ function one() {
 }
 
 one()
+
+if (true){
+    const username = "shreyansh"
+    if(username === "shreyansh"){
+        const website = "tubemate"
+        console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// ++++++++++++++++++++interesting++++++++++++++
+
+console.log(addone(4));
+
+function addone(num){
+    return num+1
+}
+
+
+// function stored in variable can't be used before initialisation
+// console.log(addtwo(4));
+
+const addtwo = function(num){
+    return num + 2
+}
+
